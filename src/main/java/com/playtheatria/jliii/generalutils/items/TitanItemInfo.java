2d3a2145fad44f75295221690d8ff466f40eb9f2
jl-iii <jesselopez1162@gmail.com>
2023-08-20 12:20:51 -0500

    add throws exception to getCharge method

--- src/main/java/com/playtheatria/jliii/generalutils/items/TitanItemInfo.java
+++ src/main/java/com/playtheatria/jliii/generalutils/items/TitanItemInfo.java
@@ -37,7 +37,7 @@ public class TitanItemInfo {
         return color.getDarkColorCode() + "  " + CHARGE_PREFIX + color.getBrightColorCode() + " " + amount;
     }
 
-    public static int getCharge(@NotNull List<String> lore, int offset) {
+    public static int getCharge(@NotNull List<String> lore, int offset) throws NumberFormatException {
         for (String string : lore) {
             if (string.contains(CHARGE_PREFIX)) {
                 return Integer.parseInt(string.substring(CHARGE_PREFIX.length() + offset));
