d8d84784ceebacd744aa50b46e45b1a796151491
JL-III <jesselopez1162@gmail.com>
2023-09-10 10:30:06 -0500

    move titan tool logic into titan tool plugin

--- src/main/java/com/playtheatria/jliii/generalutils/managers/CommandManager.java
+++ src/main/java/com/playtheatria/jliii/generalutils/managers/CommandManager.java
@@ -2,7 +2,6 @@ package com.playtheatria.jliii.generalutils.managers;
 
 import com.playtheatria.jliii.generalutils.GeneralUtils;
 import com.playtheatria.jliii.generalutils.commands.AdminCommands;
-import com.playtheatria.jliii.generalutils.commands.KitCommands;
 import com.playtheatria.jliii.generalutils.utils.CustomLogger;
 import com.playtheatria.jliii.generalutils.utils.PlayerMessenger;
 
@@ -14,16 +13,15 @@ public class CommandManager {
     private final CustomLogger customLogger;
     private final PlayerMessenger playerMessenger;
 
-    public CommandManager(GeneralUtils plugin, ConfigManager configManager, CustomLogger customLogger, PlayerMessenger playerMessenger) {
+    public CommandManager(GeneralUtils plugin, CustomLogger customLogger, PlayerMessenger playerMessenger) {
         this.plugin = plugin;
         this.customLogger = customLogger;
         this.playerMessenger = playerMessenger;
-        registerCommands(configManager);
+        registerCommands();
     }
 
-    private void registerCommands(ConfigManager configManager) {
-        Objects.requireNonNull(plugin.getServer().getPluginCommand("gu")).setExecutor(new AdminCommands(plugin, configManager, playerMessenger));
-        Objects.requireNonNull(plugin.getServer().getPluginCommand("tkit")).setExecutor(new KitCommands(configManager, customLogger));
+    private void registerCommands() {
+        Objects.requireNonNull(plugin.getServer().getPluginCommand("gu")).setExecutor(new AdminCommands(plugin, playerMessenger));
         customLogger.sendLog("Registered commands.");
     }
 
